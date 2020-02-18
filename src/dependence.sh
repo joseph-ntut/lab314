39f79e9ffc1c5c2182b3fae144e1a9a4d14081da
joseph <t10631205@ntut.org.tw>
2020-02-18 16:43:04 +0800

    add .sh file

---
+++ src/dependence.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+echo "***********************************************************************"
+echo "                      Installing jsk-recognition-msgs"
+sudo apt-get install ros-melodic-jsk-recognition-msgs
+echo "                      Installation complete"
+echo "***********************************************************************"
+
+echo "***********************************************************************"
+echo "                      Installing ceres-solver"
+sudo apt-get install ros-melodic-ceres-solver
+echo "                      Installation complete"
+echo "***********************************************************************"
+
+echo "***********************************************************************"
+echo "                      Installing nema-msgs"
+sudo apt-get install ros-melodic-nema-msgs
+echo "                      Installation complete"
+echo "***********************************************************************"
+
+echo "***********************************************************************"
+echo "                      Installing hector-map-tools"
+sudo apt-get install ros-melodic-hector-map-tools
+echo "                      Installation complete"
+echo "***********************************************************************"
+
+echo "***********************************************************************"
+echo "                      Installing jsk-rviz-plugins"
+sudo apt-get install ros-melodic-jsk-rviz-plugins
+echo "                      Installation complete"
+echo "***********************************************************************"
+
+echo "***********************************************************************"
+echo "                      Installing map-server"
+sudo apt-get install ros-melodic-map-server
+echo "                      Installation complete"
+echo "***********************************************************************"
+
+echo "***********************************************************************"
+echo "                      Installing  libpcap-dev"
+sudo apt-get install libpcap-dev
+echo "                      Installation complete"
+echo "***********************************************************************"
+
+echo "***********************************************************************"
+echo "                      Installing velodyne-pointcloud"
+sudo apt-get install ros-melodic-velodyne-pointcloud
+echo "                      Installation complete"
+echo "***********************************************************************"
+
+echo "***********************************************************************"
+echo "                      Installing tf2-sensor-msgs"
+sudo apt-get install ros-melodic-tf2-sensor-msgs
+echo "                      Installation complete"
+echo "***********************************************************************"
+
+echo "***********************************************************************"
+echo "                      Installing costmap-converter"
+sudo apt-get install ros-melodic-costmap-converter
+echo "                      Installation complete"
+echo "***********************************************************************"
+
+
+
+
