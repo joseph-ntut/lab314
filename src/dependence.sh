66673ee9cb5d3af73ac7825e018d3154a0894822
ligion8711211 <58800152+ligion8711211@users.noreply.github.com>
2020-02-27 14:33:40 +0800

    Add files via upload

--- src/dependence.sh
+++ src/dependence.sh
@@ -60,6 +60,55 @@ sudo apt-get install ros-melodic-costmap-converter
 echo "                      Installation complete"
 echo "***********************************************************************"
 
+echo "***********************************************************************"
+echo "                      Installing ddynamic_reconfigure"
+sudo apt-get install ros-melodic-ddynamic-reconfigure
+echo "                      Installation complete"
+echo "***********************************************************************"
+
+echo "***********************************************************************"
+echo "                      Installing rtabmap"
+sudo apt-get install ros-melodic-rtabmap-ros
+sudo apt-get remove ros-melodic-rtabmap ros-melodic-rtabmap-ros
+cd ~
+git clone https://github.com/introlab/rtabmap.git rtabmap
+cd rtabmap/build
+cmake ..
+make
+sudo make install
+cd ~/catkin_ws
+git clone https://github.com/introlab/rtabmap_ros.git src/rtabmap_ros
+catkin_make
+echo "                      Installation complete"
+echo "***********************************************************************"
+
+echo "***********************************************************************"
+echo "                      Installing imu_filter"
+sudo apt-get install ros-melodic-imu-filter-madgwick
+echo "                      Installation complete"
+echo "***********************************************************************"
+
+echo "***********************************************************************"
+echo "                      Installing realsense_camera"
+sudo apt-key adv --keyserver keys.gnupg.net --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE || sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE
+sudo add-apt-repository "deb http://realsense-hw-public.s3.amazonaws.com/Debian/apt-repo bionic main" -u
+sudo apt-get install librealsense2-dkms
+sudo apt-get install librealsense2-utils
+sudo apt-get install librealsense2-dev
+sudo apt-get install librealsense2-dbg
+echo "                      Installation complete"
+echo "***********************************************************************"
+
+
+
+
+
+
+
+
+
+
+
 
 
 
