376838b7918ca2a12597e164da928c5c19ffe699
ligion8711211 <58800152+ligion8711211@users.noreply.github.com>
2020-03-06 15:39:46 +0800

    Add files via upload
    vision part

---
+++ src/demo/control_mode/src/initial_pose_select.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+#include "ros/ros.h"
+#include "geometry_msgs/Twist.h"
+#include "geometry_msgs/Pose.h"
+#include "geometry_msgs/PoseStamped.h"
+#include "nav_msgs/Odometry.h"
+#include "std_msgs/Float64.h"
+#include "std_msgs/Float32.h"
+#include "std_msgs/Int8.h"
+#include "std_msgs/Bool.h"
+#include "std_srvs/Empty.h"
+
